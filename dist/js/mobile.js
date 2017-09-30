0cda29cb938236f612dcc6ff5f9994e0d717bd55
Wmaileq <bkirilenok@gmail.com>
2017-09-30 15:23:35 +0300

    Добавлен блок "Другие развлечения"

--- dist/js/mobile.js
+++ dist/js/mobile.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    for (var i = 0; i < 4; i++) {
+    for (var i = 0; i < 5; i++) {
     var q = $('.mobile-quest-slider:eq(' + i + ')').children().length;
     $('.mobile-quest-wrapper:eq(' + i + ')').width(q * 25 + 'rem');
 };
