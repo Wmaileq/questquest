de3c92d3ada7e74bdd2c264f3f20eb9e91c02b74
Wmaileq <bkirilenok@gmail.com>
2017-10-12 17:19:17 +0300

    Commit asdq123

--- dist/js/mobile.js
+++ dist/js/mobile.js
@@ -1,27 +1,25 @@
 $(document).ready(function(){
-    $('.scrollbar-inner').scrollbar();
-});
-$(document).ready(function(){
-    for (var i = 0; i < 5; i++) {
-    var q = $('.mobile-quest-slider:eq(' + i + ')').children().length;
+  // стилизация скролл-баров
+  $('.scrollbar-inner').scrollbar();
+
+  // Ширина, скрытие и начальный скролл
+  $('.mobile-quest-slider').each(function(){
+    var q = $(this).children().length;
     if ( q != 0 ) {
-    $('.mobile-quest-wrapper:eq(' + i + ')').width(q * 26.2 + 'rem');
-  } else {
-    $('.mobile-quest-wrapper:eq(' + i + ')').parent().parent().hide();
-  }
-};
+      $(this).parent().width(q * 26.2 + 'rem');
+    } else {
+      $(this).parent().parent().parent().hide();
+    }
+  });
+  $('.mobile-quest-viewport').scrollLeft(230);
+  $('.mobile-banner-view').scrollLeft(70);
+  // стрелочка подробнее
+  $('.more').click(function(e){
+    e.preventDefault();
+    $(this).hide();
+    $(this).parent().css('max-height', 'inherit');
+    $(this).parent().css('-webkit-line-clamp', 'inherit');
+    $(this).parent().css('-webkit-box-orient', 'inherit');
+    $(this).parent().parent().css('height', 'auto');
+  });
 });
-$(document).ready(function(){
-    $('.mobile-quest-viewport').scrollLeft(230);
-    $('.mobile-banner-view').scrollLeft(70);
-});
-$(document).ready(function(){
-    $('.more').click(function(e){
-  e.preventDefault();
-  $(this).hide();
-  $(this).parent().css('max-height', 'inherit');
-  $(this).parent().css('-webkit-line-clamp', 'inherit');
-  $(this).parent().css('-webkit-box-orient', 'inherit');
-  $(this).parent().parent().css('height', 'auto');
-});
-    });
